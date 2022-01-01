1af76a675073ad322538972abb2cf2e4524e2a3e
zlolwi <67956826+zlolwi@users.noreply.github.com>
2022-01-01 22:10:46 +0900

    Update c_2557.c

--- 입출력/c_2557.c
+++ 입출력/c_2557.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 int main() {
+    
+    // printf()
+    // 메모리: 1112KB, 코드 길이: 75B
     printf("Hello World!");
+    
+    // puts()
+    // 메모리: 1112KB, 코드 길이: 73B
+    puts("Hello World!");
+    
+    // fputs()
+    // 메모리: 1112KB, 코드 길이: 82B
+    fputs("Hello World!", stdout);
+    
     return 0;
+
 }
