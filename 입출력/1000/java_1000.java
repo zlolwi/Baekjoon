24c566f95005a2287f97c65c0b8e8c5b23de9083
zlolwi <67956826+zlolwi@users.noreply.github.com>
2022-01-02 23:14:28 +0900

    Create java_1000.java

---
+++ 입출력/1000/java_1000.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args){
+      // 메모리: 17724KB, 코드 길이: 248B, 시간: 212ms
+      Scanner sc = new Scanner(System.in);
+      int A,B;
+      A = sc.nextInt();
+      B = sc.nextInt();
+        
+      System.out.println(A+B); 
+    }
+}
