20f07f7e58894b55fe3a628411b233aa55468edf
zlolwi <67956826+zlolwi@users.noreply.github.com>
2022-01-03 21:05:25 +0900

    Create java_2558.java

---
+++ 입출력/2558/java_2558.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+// 메모리: 17720KB, 코드 길이: 240B, 시간: 208ms
+class Main {
+    public static void main(String[] args) {
+        int A,B;
+        Scanner sc = new Scanner(System.in);
+        A = sc.nextInt();
+        B = sc.nextInt();
+        System.out.println(A+B); 
+    }
+}
