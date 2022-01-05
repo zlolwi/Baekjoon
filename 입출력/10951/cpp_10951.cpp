20f9afe7c17465d3e706280432628cd7cfbd044c
zlolwi <67956826+zlolwi@users.noreply.github.com>
2022-01-05 16:13:19 +0900

    Create cpp_10951.cpp

---
+++ 입출력/10951/cpp_10951.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+// 메모릴 크기: 2020KB, 코드 길이: 148B, 시간: 4ms
+int main() {
+    int A,B;
+    while(!(std::cin >> A >> B).eof()){
+        std::cout << A+B << "\n";
+    } 
+    
+    return 0;
+}
