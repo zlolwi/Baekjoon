3474ee8cf78b5929186f807b2709f91aa8fb9479
zlolwi <67956826+zlolwi@users.noreply.github.com>
2022-01-05 16:22:15 +0900

    Create java_10951.java

---
+++ 입출력/10951/java_10951.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+// 메모리크기: 17872KB, 코드 길이: 290B, 시간: 224ms
+class Main {
+    public static void main(String[] args) {
+        int A,B;
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            A=sc.nextInt();
+            B=sc.nextInt();
+            System.out.println(A+B);    
+        }
+    }
+}
