600aca2923d1f85e042a1d1587d56beb91575e1e
zlolwi <67956826+zlolwi@users.noreply.github.com>
2022-01-10 23:24:40 +0900

    Create java_11718.java

---
+++ 입출력/11718/java_11718.java
@@ -0,0 +1,12 @@
+// 메모리: 17952KB, 코드 길이: 262B, 시간: 240ms
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()){
+            String input = sc.nextLine();
+            System.out.println(input); 
+        }
+    }
+}
