429d647021fc451d90c3e7c86b513ece9b106016
zlolwi <67956826+zlolwi@users.noreply.github.com>
2022-01-11 18:39:01 +0900

    Create java_11719.java

---
+++ 입출력/11719/java_11719.java
@@ -0,0 +1,14 @@
+// 메모리: 17884KB, 시간: 244ms
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        while(sc.hasNextLine()){
+            String a = sc.nextLine();
+            System.out.println(a); 
+        }
+        sc.close();
+    }
+}
