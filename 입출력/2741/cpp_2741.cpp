4d375ad0007f8962615c8c1dd5bea49c38015ce2
zlolwi <67956826+zlolwi@users.noreply.github.com>
2022-01-14 16:31:10 +0900

    Create cpp_2741.cpp

---
+++ 입출력/2741/cpp_2741.cpp
@@ -0,0 +1,13 @@
+// 메모리: 2020KB, 코드 길이: 149B, 시간: 12ms
+#include <iostream>
+using namespace std;
+
+int main() {
+   int N,i;
+   cin >> N;
+   for(i=1;i<=N;i++){
+       cout << i << "\n";
+   }
+
+    return 0;
+}
