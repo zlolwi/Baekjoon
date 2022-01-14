a71ee8ab71c4c454c23e358c1bc84ae48b88b585
zlolwi <67956826+zlolwi@users.noreply.github.com>
2022-01-14 16:46:31 +0900

    Create java_2742.java

---
+++ 입출력/2742/java_2742.java
@@ -0,0 +1,12 @@
+// 메모리: 31660KB, 코드 길이: 242B, 시간: 1232ms
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i=N;i>0;i--){
+            System.out.println(i);
+        }
+    }
+}
