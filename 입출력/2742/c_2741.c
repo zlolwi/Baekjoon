3a78132a91cf34bc75ae2ef48ea6096135dd5f1b
zlolwi <67956826+zlolwi@users.noreply.github.com>
2022-01-14 16:41:17 +0900

    Create c_2741.c

---
+++ 입출력/2742/c_2741.c
@@ -0,0 +1,12 @@
+// 메모리: 1112KB, 코드 길이: 141B, 시간: 12ms
+#include <stdio.h>
+
+int main() {
+    int N,i;
+    scanf("%d",&N);
+    for(i=N;i>=1;i--){
+        printf("%d\n",i);
+    }
+    
+    return 0;
+}
