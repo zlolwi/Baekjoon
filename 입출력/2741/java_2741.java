d7020356ca4475cc560df006e63e82a78ea0536c
zlolwi <67956826+zlolwi@users.noreply.github.com>
2022-01-14 16:35:07 +0900

    Create java_2741.java

---
+++ 입출력/2741/java_2741.java
@@ -0,0 +1,12 @@
+// 메모리: 31836KB, 코드 길이: 243B, 시간: 1020ms
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i=1;i<=N;i++){
+            System.out.println(i);
+        }
+    }
+}
