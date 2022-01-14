5c0ee636fcc62b6ae6cb83cc5401a6d0bbb0ef0b
zlolwi <67956826+zlolwi@users.noreply.github.com>
2022-01-14 16:52:59 +0900

    Create cpp_2739.cpp

---
+++ 입출력/2739/cpp_2739.cpp
@@ -0,0 +1,13 @@
+// 메모리: 2020KB, 코드 길이: 179B
+#include <iostream>
+using namespace std;
+
+int main() {
+   int N,i;
+   cin >> N;
+   for(i=1;i<=9;i++){
+       cout << N << " * " << i << " = " << i*N << "\n";
+   }
+
+    return 0;
+}
